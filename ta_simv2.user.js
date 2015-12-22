331627846bd104721e3ee79fb7155e7196a93e94
unknown <mlj1985@gmx.de>
2015-12-22 20:31:36 +0100

    sub-version update

--- ta_simv2.user.js
+++ ta_simv2.user.js
@@ -2,7 +2,7 @@
 // @name            Tiberium Alliances Battle Simulator V2
 // @description     Allows you to simulate combat before actually attacking.
 // @author          Eistee & TheStriker & VisiG & Lobotommi
-// @version         15.12.22
+// @version         15.12.22b
 // @namespace       https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
 // @include         https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
 // @icon            http://eistee82.github.io/ta_simv2/icon.png
